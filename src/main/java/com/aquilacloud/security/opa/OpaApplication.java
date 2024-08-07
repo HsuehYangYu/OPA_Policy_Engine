5b4d99a92d2de2bce2ea1165fff8c00c7b107c6f
Jimyu <jimyu@Jimyus-MacBook-Pro.local>
2024-08-07 17:06:48 -0400

    Initial commit

---
+++ src/main/java/com/aquilacloud/security/opa/OpaApplication.java
@@ -0,0 +1,13 @@
+package com.aquilacloud.security.opa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aquilacloud/security/opa/config/OpaConfiguration.java
@@ -0,0 +1,23 @@
+package com.aquilacloud.security.opa.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(OpaProperties.class)
+public class OpaConfiguration {
+    
+    private final OpaProperties opaProperties;
+
+    @Bean
+    public WebClient opaWebClient(WebClient.Builder builder) {
+
+        return builder.baseUrl(opaProperties.getEndpoint())
+            .build();
+    }
+}
